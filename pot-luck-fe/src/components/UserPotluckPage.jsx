b7f3ce701f34556643b01302b668f499d2b9420a
Mychael M <mychaelbmenges@gmail.com>
2020-11-11 19:29:48 -0700

    added dummy data and rendered mock potluck data on UserPotluckPage.jsx

--- pot-luck-fe/src/components/UserPotluckPage.jsx
+++ pot-luck-fe/src/components/UserPotluckPage.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import data from '../data';
+import styled from 'styled-components';
+
+const Potluck = styled.div`
+border: 1px solid black;
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 20%;
+margin: 0 auto;
+`
 
 const UserPotluckPage = () => {
 
   return (
-    <h1>User Profile Page</h1>
+    data.map(potluck => {
+      return (
+        <Potluck>
+      <h1>{potluck.title}</h1>
+      {potluck.items.map(item => {
+        return(
+          <ul>{item}</ul>
+          )
+        }
+        )}
+        <p>{potluck.datetime}</p>
+        <p>{potluck.location}</p>
+      </Potluck>
+    )})
   )
 }
 
---
+++ pot-luck-fe/src/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    title: "Test Title",
+    datetime: "11/11/2020 4:30 PM",
+    location: "Nowhere",
+    items: ["food", "drinks", "condiments"]
+  },
+  {
+    title: "Test Title 2",
+    datetime: "11/11/2020 4:30 PM",
+    location: "Nowhere",
+    items: ["food", "drinks", "condiments"]
+  },
+  {
+    title: "Test Title 3",
+    datetime: "11/11/2020 4:30 PM",
+    location: "Nowhere",
+    items: ["food", "drinks", "condiments"]
+  },
+]
+
+export default data;
