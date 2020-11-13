a4fd6d93f3d9d25d6f914cbcf68a46b844eb93cf
unknown <ngriffith29@student.egcc.edu>
2020-11-12 20:37:26 -0500

    sign up is setup enough to test on

--- pot-luck-fe/src/components/SignUp.jsx
+++ pot-luck-fe/src/components/SignUp.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, {useState} from 'react'
+import SelectUSState from 'react-select-us-states';
 
 function SignUp() {
+    const [formData, setFormData] = useState({
+        fName: "",
+        lName: "",
+        email: "",
+        password: "",
+        imageURL: "",
+        Location: "",
+    });
+
+    const onInputChange = event => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+            
+        
+        });
+    }
+const statePick = (newVal) => {
+    setFormData({
+        ...formData,
+        [formData.Location]: newVal
+    })
+}
+
+const submit = (event) => {
+    event.preventDefault();
+    setFormData({   fName: "",
+    lName: "",
+    email: "",
+    password: "",
+    imageURL: "",
+    Location: "",})
+    
+
+}
+  
     return (
         <div>
-            <h1>Sign Up</h1>
+            <h1>Sign Up!</h1>
+            <form onSubmit={submit}>
+                <label>
+                First Name
+                <input name="fName" type="text" onChange={onInputChange}  value={formData.fName}/>
+                </label>
+                <label>
+                Last Name
+                <input name="lName" type="text" onChange={onInputChange}  value={formData.lName}/>
+                </label>
+                <label>
+                Email
+                <input name="email" type="text" onChange={onInputChange}  value={formData.email}/>
+                </label>
+                <label>
+                Password
+                <input name="password" type="password" onChange={onInputChange}value={formData.password}/>
+                </label>
+                <label>
+                <br />  Location
+               <SelectUSState  onChange={statePick}/>
+      
+               
+                </label>
+
+                
+                <label>
+                Image Url   
+                <input name="imageURL" type="text" onChange={onInputChange} value={formData.imageURL} />
+                </label>
+
+
+
+            <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
