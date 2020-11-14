1a6a59e404ddcf842307b4267d0d231f4517550a
Mychael M <mychaelbmenges@gmail.com>
2020-11-14 15:23:28 -0700

    added react datepicker and required text fields to new potluck form

--- pot-luck-fe/src/components/NewPotluckForm.jsx
+++ pot-luck-fe/src/components/NewPotluckForm.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axiosWithAuth from '../utils/axiosWithAuth';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 const NewPotluckForm = () => {
+  const [startDate, setStartDate] = useState(new Date());
 
   return (
-    <h1>New Potluck Form</h1>
+    <div>
+    <h1>Add New Potluck</h1>
+    <form>
+      <input 
+      type="text"
+      name="title"
+      id="title"
+      placeholder="Title"
+      />
+      <br/>
+
+      <input 
+      type="text"
+      name="location"
+      id="location"
+      placeholder="Location"
+      />
+      <br/>
+
+      <textarea 
+      name="items"
+      id="items"
+      placeholder="Add new items separated by commas"
+      />
+      <br/>
+
+      <DatePicker
+      selected={startDate}
+      onChange={date => setStartDate(date)}
+      timeInputLabel="Time:"
+      dateFormat="MM/dd/yyyy h:mm aa"
+      showTimeInput
+    />
+
+    <button>Submit</button>
+    
+    </form>
+    </div>
   )
 };
 
