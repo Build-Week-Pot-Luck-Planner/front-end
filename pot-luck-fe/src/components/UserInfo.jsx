a7639214e420e009ce72752a907bcb719a40ad16
Mychael M <mychaelbmenges@gmail.com>
2020-11-15 16:59:54 -0700

    added profile Img field to user info

--- pot-luck-fe/src/components/UserInfo.jsx
+++ pot-luck-fe/src/components/UserInfo.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ProfileImg = styled.img`
+margin-top: 2%;
+border-radius: 100%;
+height: 100px;
+`
 
 const UserInfo = (props) => {
 
   return (
     <div>
-    <h2>User Profile Information</h2>
+    <ProfileImg src={props.user.pfp} alt="User Profile img"/>
+    {/* <h2>User Profile Information</h2> */}
     <h3>Username: {props.user.username}</h3>
     <p>Email: {props.user.email}</p>
     <p>Location: {props.user.location}</p>
